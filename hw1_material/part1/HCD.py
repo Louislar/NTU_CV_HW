997648767c68a4a8fc5f4006b5261ec3f7a1868f
Louislar <a8945267@gmail.com>
2021-03-17 00:47:40 +0800

    完成作業1的part1的最基本部份了
    還有part1的報告要寫

--- hw1_material/part1/HCD.py
+++ hw1_material/part1/HCD.py
@@ -27,17 +27,20 @@ class Harris_corner_detector(object):
         ])
         Ix = cv2.filter2D(smooth_image, -1, kernel_for_Ix)
         Iy = cv2.filter2D(smooth_image, -1, kernel_for_Iy)
+        print(Ix.dtype)
 
         # Step 3: Compute Ixx, Ixy, Iyy (Ixx = Ix*Ix, ...)
-        Ixx = np.dot(Ix, Ix) # 這邊是dot product還是Hadamard product?
-        Ixy = np.dot(Ix, Iy)
-        Iyy = np.dot(Iy, Iy)
+        Ixx = np.multiply(Ix, Ix) # 這邊是dot product還是Hadamard product?
+        Ixy = np.multiply(Ix, Iy)
+        Iyy = np.multiply(Iy, Iy)
+        print(Ixx.dtype)
 
         # Step 4: Compute Sxx, Sxy, Syy (weighted summation of Ixx, Ixy, Iyy in neighbor pixels)
         # - Function: cv2.GaussianBlur (kernel = 3, sigma = 1.)
         Sxx = cv2.GaussianBlur(Ixx, (3, 3), 1.)
         Sxy = cv2.GaussianBlur(Ixy, (3, 3), 1.)
         Syy = cv2.GaussianBlur(Iyy, (3, 3), 1.)
+        print(Sxx.dtype)
 
         # Step 5: Compute the det and trace of matrix M (M = [[Sxx, Sxy], [Sxy, Syy]])
         det = np.zeros(Sxx.shape)
@@ -47,18 +50,42 @@ class Harris_corner_detector(object):
                 M = np.array([[Sxx[i, j], Sxy[i, j]], [Sxy[i, j], Syy[i, j]]])
                 det[i, j] = np.linalg.det(M)
                 trace[i, j] = np.trace(M)
-
-        # print(Sxx.shape)  # (256, 256)
+        print(det.dtype)
+        print(trace.dtype)
 
         # Step 6: Compute the response of the detector by det/(trace+1e-12)
         R_matrix = np.zeros(Sxx.shape)
         for i in range(Sxx.shape[0]): 
             for j in range(Sxx.shape[1]): 
-                R_matrix[i, j] = det[i, j]/trace[i, j]
+                R_matrix[i, j] = det[i, j]/(trace[i, j]+10**-12)
+        print(R_matrix.dtype)
 
         # print(R_matrix.shape) # (256, 256)
 
-        cv2.imshow('img_show', R_matrix)
+        # return 0
+        return R_matrix
+    
+    def post_processing(self, response):
+        ### TODO ###
+        # Step 1: Thresholding
+        retval, after_thresholding = cv2.threshold(response, self.threshold, 255., cv2.THRESH_BINARY)
+        candidate_pt_list = list(zip(*np.where(after_thresholding==255.)))
+        # Step 2: Find local maximum
+        local_maximum_list = []
+        after_padding = cv2.copyMakeBorder(response, 2, 2, 2, 2, cv2.BORDER_CONSTANT, 0)
+        for i, j in candidate_pt_list: 
+            i = i+2
+            j = j+2
+            tmp_mat = after_padding[i-2:i+3, j-2:j+3]
+            central_val = after_padding[i, j]
+            maximum_value = np.amax(tmp_mat)
+            # print(np.isin(maximum_value, np.unique(tmp_mat)))
+            if central_val >= maximum_value: 
+                if np.isin(maximum_value, np.unique(tmp_mat)): 
+                    local_maximum_list.append([i-2, j-2])
+
+        # print(local_maximum_list)
+        cv2.imshow('img_show', after_padding)
         # 按空白鍵退出
         key = None
         while True: 
@@ -69,18 +96,4 @@ class Harris_corner_detector(object):
         cv2.destroyAllWindows()
 
         # return 0
-        return R_matrix
-    
-    def post_processing(self, response):
-        ### TODO ###
-        # Step 1: Thresholding
-        after_thresholding = cv2.threshold(response, self.threshold, 255., cv2.THRESH_BINARY)
-        print(response[1:10, 1:10])
-        print(np.sum(response>=self.threshold))
-        print(np.where(response>=self.threshold))
-        print(np.where(after_thresholding==255.))
-        # Step 2: Find local maximum
-
-
-        return 0
-        # return local_max
+        return local_maximum_list
