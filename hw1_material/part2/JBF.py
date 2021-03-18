d881b1f39d4b428d9944fc32a3c7eac63d1ff435
Louislar <a8945267@gmail.com>
2021-03-18 16:41:36 +0800

    完成最naiive的joint bilater filter了

--- hw1_material/part2/JBF.py
+++ hw1_material/part2/JBF.py
@@ -22,19 +22,19 @@ class Joint_bilateral_filter(object):
         guidance_img_dim = padded_guidance.ndim
 
         # 1. gaussian kernel lookup table 
-        
         grid_x, grid_y = np.meshgrid(
             np.linspace(-(half_window_len), half_window_len, self.wndw_size, dtype=np.float64), 
             np.linspace(-(half_window_len), half_window_len, self.wndw_size, dtype=np.float64)
         )
-        gaussian_kernel = np.exp(-1*(grid_x**2 + grid_y**2 / (2*self.sigma_s**2)))
-        gaussian_kernel_3d = np.repeat(gaussian_kernel[:, :, np.newaxis], 3, axis=2)
+        gaussian_kernel = np.exp(-1*((grid_x**2 + grid_y**2) / (2*self.sigma_s**2)))
+        gaussian_kernel_3d = gaussian_kernel[:, :, None]
         # print(gaussian_kernel)
         # print(gaussian_kernel.dtype)
 
         # 2.1 guidence image normalize [0, 1]
         normalized_padded_guidance = padded_guidance / 255.
-        # 2.2 range kernel lookup table
+        # 2.2 range kernel lookup table (how to do this?)
+        #       Note: range kernel will only use guidance image to calculate kernel entries
         
 
         # 3. convolution (naiive solution)
@@ -43,40 +43,43 @@ class Joint_bilateral_filter(object):
             i = i + half_window_len
             for j in range(padded_guidance.shape[1]-2*half_window_len):
                 j = j + half_window_len
+                window_pixels = padded_img[i-half_window_len:i+half_window_len+1, j-half_window_len:j+half_window_len+1, :]
+                # print(i, ', ', j, ', ', i-half_window_len, ', ', i+half_window_len+1, ', ', j-half_window_len, ', ', j+half_window_len+1)
+
                 if guidance_img_dim == 3: 
                     central_val = normalized_padded_guidance[i,j,:]
                     normalized_window_pixels = normalized_padded_guidance[i-half_window_len:i+half_window_len+1, j-half_window_len:j+half_window_len+1, :]
-                    window_pixels = padded_guidance[i-half_window_len:i+half_window_len+1, j-half_window_len:j+half_window_len+1, :]
-                    # print(i, ', ', j, ', ', i-half_window_len, ', ', i+half_window_len+1, ', ', j-half_window_len, ', ', j+half_window_len+1)
-                    for k in range(3): 
-                        normalized_window_pixels[:,:,k] = (normalized_window_pixels[:,:,k]-central_val[k])**2
-
-                    sum_of_rgb = normalized_window_pixels[:,:,0] + normalized_window_pixels[:,:,1] + normalized_window_pixels[:,:,2]
-                    final_range_kernel = np.exp((-sum_of_rgb)/(2*self.sigma_r))
-                    # print(final_range_kernel.shape)
-
-                    # spatial, range kernel調整成三維
-                    # ref: https://stackoverflow.com/questions/32171917/copy-2d-array-into-3rd-dimension-n-times-python
-                    range_kernel_3d = np.repeat(final_range_kernel[:, :, np.newaxis], 3, axis=2)
-                    # print(range_kernel_3d.shape)
-                    
-                    # 三個矩陣element wise相乘，最後加總 
-                    # TODO: 感覺不大對，range kernel已經將rgb合併在一起，現在又全部展開成3維 (range kernel在算的是兩個pixel的距離)
-                    after_multiply_fraction = np.multiply(np.multiply(gaussian_kernel_3d, range_kernel_3d), window_pixels)
+                    # calculate range kernel
+                    normalized_window_pixels = np.power((normalized_window_pixels-central_val), 2)
+                    sum_of_squ_of_rgb = np.sum(normalized_window_pixels, axis=2)
+                    final_range_kernel = np.exp((-sum_of_squ_of_rgb)/(2*self.sigma_r**2))
+                    range_kernel_3d = final_range_kernel[:, :, None]    # range kernel調整成三維
                     after_multiply_denominator = np.multiply(gaussian_kernel_3d, range_kernel_3d)
-                    after_divide = np.divide(after_multiply_fraction, after_multiply_denominator)
-                    
-                    # print(after_multiply.shape)
 
-                    for k in range(3): 
-                        after_conv_mat[i-half_window_len, j-half_window_len, k] = \
-                            after_divide[k].sum()
+
+                # guidance image 是灰階圖 --> 2維陣列
+                elif guidance_img_dim == 2: 
+                    central_val = normalized_padded_guidance[i,j]
+                    normalized_window_pixels = normalized_padded_guidance[i-half_window_len:i+half_window_len+1, j-half_window_len:j+half_window_len+1]
+                    normalized_window_pixels = np.power((normalized_window_pixels - central_val), 2)
+                    final_range_kernel = np.exp(-(normalized_window_pixels)/(2*self.sigma_r**2))
+                    after_multiply_denominator = np.multiply(gaussian_kernel, final_range_kernel)
+                    after_multiply_denominator = after_multiply_denominator[:, :, None]
+
+                after_multiply_fraction = np.multiply(after_multiply_denominator, window_pixels)
+                after_sum_denominator = np.sum(after_multiply_denominator, axis=(0, 1))
+                after_sum_fraction = np.sum(after_multiply_fraction, axis=(0, 1))
+                after_divide = np.divide(after_sum_fraction, after_sum_denominator)
+                after_conv_mat[i-half_window_len, j-half_window_len, :] = after_divide 
+
         
         print(after_conv_mat.shape)
+        print(after_conv_mat[0:20, 0:20])
+        print(after_conv_mat.dtype)
         
 
 
-        cv2.imshow('img_show', after_conv_mat)
+        cv2.imshow('img_show', cv2.cvtColor(np.clip(after_conv_mat, 0, 255).astype(np.uint8), cv2.COLOR_RGB2BGR))
         # 按空白鍵退出
         key = None
         while True: 
