7a9a2eca83a25dd5c299c6d490b90c7ffbcf2654
Louislar <a8945267@gmail.com>
2021-05-23 16:05:39 +0800

    修正report上part3回答錯誤
    並且重新產生pdf

--- hw3/src/part3.py
+++ hw3/src/part3.py
@@ -11,15 +11,27 @@ if __name__ == '__main__':
     corners1 = np.array([[429, 337], [517, 314], [570, 361], [488, 380]])
     corners2 = np.array([[346, 196], [437, 161], [483, 198], [397, 229]])
     h, w, c = (500, 500, 3)
-    dst = np.zeros((h, w, c))
+    dst1 = np.zeros((h, w, c))
+    dst2 = np.zeros((h, w, c))
 
     # TODO: call solve_homography() & warping
     H1 = solve_homography(corners1, np.array([[0, 0], [w, 0], [w, h], [0, h]]))
     H2 = solve_homography(corners2, np.array([[0, 0], [w, 0], [w, h], [0, h]]))
 
 
-    output3_1 = warping(secret1, dst, H1, 0, h, 0, w, 'b')
-    output3_2 = warping(secret2, dst, H2, 0, h, 0, w, 'b')
+    output3_1 = warping(secret1, dst1, H1, 0, h, 0, w, 'b')
+    output3_2 = warping(secret2, dst2, H2, 0, h, 0, w, 'b')
 
     cv2.imwrite('output3_1.png', output3_1)
     cv2.imwrite('output3_2.png', output3_2)
+    cv2.imwrite('output3_1_1.png', dst1)
+    cv2.imwrite('output3_2_1.png', dst2)
+
+    dstFull1 = np.zeros(secret1.shape)
+    dstFull2 = np.zeros(secret1.shape)
+    output3_3 = warping(secret1, dstFull1, H1, 0, dstFull1.shape[0], 0, dstFull1.shape[1], 'b')
+    output3_4 = warping(secret2, dstFull2, H2, 0, dstFull1.shape[0], 0, dstFull1.shape[1], 'b')
+    cv2.imwrite('output3_3.png', output3_3)
+    cv2.imwrite('output3_4.png', output3_4)
+    print(output3_3.shape)
+    print(output3_4.shape)
